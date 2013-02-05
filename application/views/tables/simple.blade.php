44ff5abb31220e853c8dd3e6542f36941e86fafe
Taufiq Ridha <taufiq.ridha@gmail.com>
2013-02-05 17:17:42 +0700

    style expander,sorting

--- application/views/tables/simple.blade.php
+++ application/views/tables/simple.blade.php
@@ -190,10 +190,10 @@
 
            <hr class="win-command" />
 
-           <button class="win-command" onclick="toggle_visibility('content-filters');">
+           <!--<button class="win-command" onclick="toggle_visibility('content-filters');">
               <span class="win-commandimage win-commandring">&#x0067;</span>
               <span class="win-label">Filter</span>
-           </button>
+           </button>-->
 <!--<<<<<<< 
            	@if(isset($addurl) && $addurl != '')
 				<a class="win-command" href="{{URL::to($addurl)}}">
