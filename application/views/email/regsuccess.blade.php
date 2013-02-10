5097d2d459b3cfe1da27ac1ac927e62c79d46869
Taufiq Ridha <taufiq.ridha@gmail.com>
2013-02-10 17:43:03 +0700

    pull 10 Jan

--- application/views/email/regsuccess.blade.php
+++ application/views/email/regsuccess.blade.php
@@ -15,27 +15,89 @@ Thank you for register in 37th IPA Convention & Exhibition. Please find below su
 
 <p><strong><u>CONVENTION REGISTRATION</u></strong></p>
 
-<p>Type of registration fee 
+<table>
 	@if($data['regtype'] == 'PO')
-		(Professional / Delegate Overseas)
+		<tr>
+			<td style="padding:10px;"><strong>Professional / Delegate Overseas</strong></td>
+			<td style="padding:10px;"><strong>USD 500</strong></td>
+			<td style="padding:10px;"><strong>IDR - </strong></td>
+		</tr>
 	@elseif($data['regtype'] == 'PD')
-		(Professional / Delegate Domestic)
+		<tr>
+			<td style="padding:10px;"><strong>Professional / Delegate Domestic</strong></td>
+			<td style="padding:10px;"><strong>USD - </strong></td>
+			<td style="padding:10px;"><strong>IDR 4.500.000</strong></td>
+		</tr>
 	@elseif($data['regtype'] == 'SD')
-		(Student Domestic)
+		<tr>
+			<td style="padding:10px;"><strong>Student Domestic</strong></td>
+			<td style="padding:10px;"><strong>USD - </strong></td>
+			<td style="padding:10px;"><strong>IDR 400.000</strong></td>
+		</tr>
 	@elseif($data['regtype'] == 'SO')
-		(Student Overseas)
-	@endif : 
-	@if($data['regtype'] == 'PO')
-		USD 500
-	@elseif($data['regtype'] == 'PD')
-		IDR 4.500.000,-
-	@elseif($data['regtype'] == 'SD')
-		IDR 400.000,-
-	@elseif($data['regtype'] == 'SO')
-		USD 120
+		<tr>
+			<td style="padding:10px;"><strong>Student Overseas</strong></td>
+			<td style="padding:10px;"><strong>USD 120</strong></td>
+			<td style="padding:10px;"><strong>IDR - </strong></td>
+		</tr>
 	@endif
-<br/>
-Attend on Industrial Dinner (16 May 2013): {{ $data['attenddinner'] }}
+	
+	@if($data['golf'] == 'Yes')
+		<tr>
+			<td style="padding:10px;"><strong>Golf Tournament</strong></td>
+			<td style="padding:10px;"><strong>USD - </strong></td>
+			<td style="padding:10px;"><strong>IDR 2.500.000</strong></td>
+		</tr>	
+	@else
+		<tr>
+			<td style="padding:10px;"><strong>Golf Tournament</strong></td>
+			<td style="padding:10px;"><strong>USD - </strong></td>
+			<td style="padding:10px;"><strong>IDR - </strong></td>
+		</tr>	
+	@endif
+	@if($data['regtype'] == 'PO' && $data['golf'] == 'Yes')
+		<tr>
+			<td style="padding:10px;border-bottom:1px solid #000;"><strong>Grand Total</strong></td>
+			<td style="padding:10px;border-bottom:1px solid #000;"><strong>USD 500</strong></td>
+			<td style="padding:10px;border-bottom:1px solid #000;"><strong>IDR 2.500.000</strong></td>
+		</tr>
+	@elseif($data['regtype'] == 'PD' && $data['golf'] == 'Yes')
+		<tr>
+			<td style="padding:10px;border-bottom:1px solid #000;"><strong>Grand Total</strong></td>
+			<td style="padding:10px;border-bottom:1px solid #000;"><strong>USD - </strong></td>
+			<td style="padding:10px;border-bottom:1px solid #000;"><strong>IDR 7.000.000</strong></td>
+		</tr>
+	@elseif($data['regtype'] == 'PO' && $data['golf'] == 'No')
+		<tr>
+			<td style="padding:10px;border-bottom:1px solid #000;"><strong>Grand Total</strong></td>
+			<td style="padding:10px;border-bottom:1px solid #000;"><strong>USD 500</strong></td>
+			<td style="padding:10px;border-bottom:1px solid #000;"><strong>IDR - </strong></td>
+		</tr>
+	@elseif($data['regtype'] == 'PD' && $data['golf'] == 'No')
+		<tr>
+			<td style="padding:10px;border-bottom:1px solid #000;"><strong>Grand Total</strong></td>
+			<td style="padding:10px;border-bottom:1px solid #000;"><strong>USD - </strong></td>
+			<td style="padding:10px;border-bottom:1px solid #000;"><strong>IDR 4.500.000</strong></td>
+		</tr>
+	@elseif($data['regtype'] == 'SD')
+		<tr>
+			<td style="padding:10px;border-bottom:1px solid #000;"><strong>Grand Total</strong></td>
+			<td style="padding:10px;border-bottom:1px solid #000;"><strong>USD - </strong></td>
+			<td style="padding:10px;border-bottom:1px solid #000;"><strong>IDR 400.000</strong></td>
+		</tr>
+	@elseif($data['regtype'] == 'SO')
+		<tr>
+			<td style="padding:10px;border-bottom:1px solid #000;"><strong>Grand Total</strong></td>
+			<td style="padding:10px;border-bottom:1px solid #000;"><strong>USD 120</strong></td>
+			<td style="padding:10px;border-bottom:1px solid #000;"><strong>IDR - </strong></td>
+		</tr>
+	@endif
+		<tr>
+			<td style="padding:10px;" colspan="2"><strong>Attend on Industrial Dinner (16 May 2013)</strong></td>
+			<td style="padding:10px;"><strong>{{ $data['attenddinner'] }}</strong></td>
+		</tr>
+
+</table>
 </p>
 
 <p><i>*Convention registration fee includes admission to all Plenary & Technical Sessions, Conference Kits, Opening and Closing Ceremony, Lunches, Coffee Breaks, Industrial Cocktail, Industrial Dinner, and Entrance to Exhibition Area.<br/>
@@ -71,7 +133,7 @@ Swiftcode : CENAIDJA</p>
 			<li><span style="margin:6px;display:inline-block;width:20%;">Wednesday</span> <span style="display:inline-block;width:20%;">15 May 2013</span><span style="display:inline-block;width:30%;">08.00 AM – 03.00 PM</span></li>
 			<li><span style="margin:6px;display:inline-block;width:20%;">Thursday</span> <span style="display:inline-block;width:20%;">16 May 2013</span><span style="display:inline-block;width:30%;">08.00 AM – 03.00 PM</span></li>
 			<li><span style="margin:6px;display:inline-block;width:20%;">Friday</span> <span style="display:inline-block;width:20%;">17 May 2013</span><span style="display:inline-block;width:30%;">08.00 AM – 12.00 PM</span></li>
-			<li>Participants will be able to collect the convention kits at registration counter</li>
+			<li style="margin:6px;">Participants will be able to collect the convention kits at registration counter</li>
 		</ul>
 	</li>
 	<li>Registered participants <strong>must wear ID badges</strong> all the times for sessions and function access</li>
