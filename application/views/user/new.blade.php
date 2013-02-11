3afc87e5e6850540fc2ab418ea78874d30782301
awidarto <andy.awidarto@gmail.com>
2013-02-12 01:58:46 +0700

    user forms

--- application/views/user/new.blade.php
+++ application/views/user/new.blade.php
@@ -7,47 +7,53 @@
 </div>
 
 {{$form->open('user/add','POST',array('class'=>'custom'))}}
-<div class="row">
-  <div class="six columns left">
+<div class="row-fluid">
+  <div class="span6 left">
     <h4>User Info</h4>
-    {{ $form->text('email','Email.req','',array('class'=>'text')) }}
-    {{ $form->text('fullname','Full Name.req','',array('class'=>'text')) }}
-    {{ $form->text('username','User Name.req','',array('class'=>'text')) }}
-    {{ $form->password('pass','Password','',array('class'=>'text')) }}
-    {{ $form->password('repass','Repeat Password','',array('class'=>'text')) }}
+    {{ $form->text('email','Email.req','',array('class'=>'text span8')) }}
+    {{ $form->text('fullname','Full Name.req','',array('class'=>'text span8')) }}
+    {{ $form->text('username','User Name.req','',array('class'=>'text span8')) }}
+    {{ $form->password('pass','Password','',array('class'=>'text span8')) }}
+    {{ $form->password('repass','Repeat Password','',array('class'=>'text span8')) }}
 
     <h4>Employee Info</h4>
-    {{ $form->text('employee_jobtitle','Job Title','',array('class'=>'text')) }}
+    {{ $form->text('employee_jobtitle','Job Title','',array('class'=>'text span8')) }}
     {{ Form::label('Department','department')}}
-    {{$form->select('department','',Config::get('kickstart.department'),null,array('class'=>'four'))}}
+    {{$form->select('department','',Config::get('kickstart.department'),null)}}
 
   </div>
-  <div class="five columns right">
+  <div class="span5 right">
     <h4>Contact Info</h4>
-    {{ $form->text('mobile','Mobile Number','',array('class'=>'text')) }}
-    {{ $form->text('home','Home Number','',array('class'=>'text')) }}
-    {{ $form->textarea('street','Street','',array('class'=>'text')) }}
-    {{ $form->text('city','City','',array('class'=>'text')) }}
-    {{ $form->text('zip','ZIP','',array('class'=>'text')) }}
+    {{ $form->text('mobile','Mobile Number','',array('class'=>'text span8')) }}
+    {{ $form->text('home','Home Number','',array('class'=>'text span8')) }}
+    {{ $form->textarea('street','Street','',array('class'=>'text span8')) }}
+
+
+
+    <div class="row-fluid inputInline">
+            {{ $form->text('city','','',array('class'=>'text span12','id'=>'city','placeholder'=>'City')) }}
+            {{ $form->text('zip','','',array('class'=>'text span3','id'=>'zip','placeholder'=>'ZIP Code')) }}
+    </div>
+
 
   </div>
 </div>
-  <div class="row">
+  <div class="row-fluid">
     <h4>Access Control</h4>
-    <div class="row">
-      <div class="six columns">
+    <div class="row-fluid">
+      <div class="span6">
         <h5>Role</h5>
         {{$form->select('role','',Config::get('kickstart.roles'),null,array('class'=>'four'))}}
       </div>
     </div>
-  <div class="row">
-    <div class="twelve columns">
+  <div class="row-fluid">
+    <div class="span12">
 
       <h5>Permissions</h5>
 
       <ul>
         @foreach(Config::get('kickstart.department') as $obj=>$title)
-            <li class="three columns">
+            <li class="span3">
               {{ $form->checkbox($obj,$title,1)}} 
                 <?php
                 /*
