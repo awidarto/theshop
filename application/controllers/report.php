cc8b4694f43e973e2b05d331ab019f7a68c40172
Taufiq Ridha <taufiq.ridha@gmail.com>
2013-02-19 00:17:21 +0700

    report page daily

--- application/controllers/report.php
+++ application/controllers/report.php
@@ -65,6 +65,10 @@ class Report_Controller extends Base_Controller {
 		$stat['Golf'] = $attendee->count(array('golf'=>'Yes'));
 		$stat['Dinner'] = $attendee->count(array('attenddinner'=>'Yes'));
 		$country = Config::get('country.countries');
+		$today = date('Y-m-d');
+		$getdate = $this->makeDateRange('2013-02-11', $today);
+		$getCount = $this->getCountAttendee('2013-02-11', $today);
+		
 
 		foreach ($country as $key => $value) {
 			$coutryValue[$value] = $attendee->count(array('country'=>$value));
@@ -75,9 +79,64 @@ class Report_Controller extends Base_Controller {
 			->with('stat',$stat)
 			->with('coutryValue',$coutryValue)
 			->with('country',$country)
+			->with('getdate',$getdate)
+			->with('getCount',$getCount)
+			
 			->with('crumb',$this->crumb);
 	}
 
+	private function makeDateRange($strDateFrom,$strDateTo){
+
+	  $aryRange=array();
+
+	  $iDateFrom=mktime(1,0,0,substr($strDateFrom,5,2),     substr($strDateFrom,8,2),substr($strDateFrom,0,4));
+	  $iDateTo=mktime(1,0,0,substr($strDateTo,5,2),     substr($strDateTo,8,2),substr($strDateTo,0,4));
+
+	  if ($iDateTo>=$iDateFrom) {
+	    array_push($aryRange,date('d M',$iDateFrom)); // first entry
+
+	    while ($iDateFrom<$iDateTo) {
+	      $iDateFrom+=86400; // add 24 hours
+	      array_push($aryRange,date('d M',$iDateFrom));
+	    }
+	  }
+	  return $aryRange;
+	}
+
+	private function getCountAttendee($strDateFrom,$strDateTo){
+
+	  $attendee = new Attendee();
+	  $aryRange=array();
+
+	  $iDateFrom=mktime(1,0,0,substr($strDateFrom,5,2),     substr($strDateFrom,8,2),substr($strDateFrom,0,4));
+	  $iDateTo=mktime(1,0,0,substr($strDateTo,5,2),     substr($strDateTo,8,2),substr($strDateTo,0,4));
+
+	  if ($iDateTo>=$iDateFrom) {
+	   
+	    
+	    $fromDate = date('d-M-Y',$iDateFrom);
+	    $toDate = date('d-M-Y',$iDateFrom);
+	    $dateFrom = new MongoDate(strtotime($fromDate." 00:00:00"));
+		$dateTo = new MongoDate(strtotime($toDate." 23:59:59"));
+
+		$dataresult = $attendee->count(array('createdDate'=>array('$gte'=>$dateFrom,'$lte'=>$dateTo)));
+		array_push($aryRange,$dataresult ); // first entry
+
+
+	    while ($iDateFrom<$iDateTo) {
+	      	$iDateFrom+=86400; // add 24 hours
+			$fromDate = date('d-M-Y',$iDateFrom);
+			$toDate = date('d-M-Y',$iDateFrom);
+			$dateFrom = new MongoDate(strtotime($fromDate." 00:00:00"));
+			$dateTo = new MongoDate(strtotime($toDate." 23:59:59"));
+
+			$dataresult = $attendee->count(array('createdDate'=>array('$gte'=>$dateFrom,'$lte'=>$dateTo)));
+			array_push($aryRange,$dataresult ); // first entry
+
+	    }
+	  }
+	  return $aryRange;
+	}
 }
 
 ?>
--- application/views/report/summary.blade.php
+++ application/views/report/summary.blade.php
@@ -2,10 +2,10 @@
 
 @section('content')
 <div class="metro span12">
-	<div class="metro-sections">
+	<div class="metro-sections-noeffect">
 
 	   <div id="section1" class="metro-section tile-span-8">
-	   		<div class="blockseparate">
+	   		<div class="blockseparate marginbottom">
 		      <h2>Convention Registration</h2>
 		     <!-- <h5>Convention Registration</h5> -->
 		      <a class="tile imagetext bg-color-blue statistic" href="#">
@@ -121,7 +121,7 @@
 		         </div>
 		      </a>
 	      </div>
-	      <div class="separateMetro">
+	      <div class="separateMetro marginbottom">
 	      <h2>Registration by Country</h2>
 	      <table class="report">
 	      	<?php foreach ($country as $key =>$value ) {
@@ -133,14 +133,79 @@
 		      	</tr>
 	      	<?php 
 	      		}
+
 	      	}
-	      	
 	      	?>
-	      	
 	      </table>
-	  		</div>
+	  	  </div>
+
 	      <div class="clear"></div>
-	      
+	      <?php
+			$last = array_pop($getdate);
+			$lastCount = array_pop($getCount);	
+			?>
+	      	<script type="text/javascript">
+			$(function () {
+			    var chart;
+			    $(document).ready(function() {
+			        chart = new Highcharts.Chart({
+			            chart: {
+			                renderTo: 'charts',
+			                type: 'line',
+			                marginRight: 130,
+			                marginBottom: 25
+			            },
+			            title: {
+			                text: '',
+			                x: -20 //center
+			            },
+			            
+			            xAxis: {
+                            
+			                categories: [<?php foreach ($getdate as $date) { echo "'".$date."',";} echo "'".$last."'";?>]
+
+			            },
+			            yAxis: {
+			                title: {
+			                    text: ''
+			                },
+			                plotLines: [{
+			                    value: 0,
+			                    width: 1,
+			                    color: '#808080'
+			                }]
+			            },
+			            tooltip: {
+			                formatter: function() {
+			                        return '<b>'+ this.x +'</b><br/>'+
+			                        this.y +' Registration';
+			                }
+			            },
+			            legend: {
+			                layout: 'vertical',
+			                align: 'right',
+			                verticalAlign: 'top',
+			                x: -10,
+			                y: 100,
+			                borderWidth: 0
+			            },
+			            series: [
+			            {
+			                name: 'Attendee',
+			                data: [<?php foreach ($getCount as $att) { echo $att.",";} echo $lastCount;?>]
+			            }
+			            ]
+			        });
+			    });
+			    
+			});
+			</script>
+            {{ HTML::script('js/highcharts.js') }}
+            {{ HTML::script('js/highcharts-exporting.js') }}
+            <h2>Daily Registration</h2>
+	    	<div id="charts" style="min-width: 400px; height: 400px; margin: 0 auto">
+
+            </div>
 	   </div>
 
 		
